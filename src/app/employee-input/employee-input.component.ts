b284146ae4045037a249a28b9f8e5c75d4fde41f
Ankur Israni <ankur.israni@aa.com>
2019-08-06 13:31:43 -0500

    first commit

---
+++ src/app/employee-input/employee-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-input',
+  templateUrl: './employee-input.component.html',
+  styleUrls: ['./employee-input.component.scss']
+})
+export class EmployeeInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
